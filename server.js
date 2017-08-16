1cc631b61dfa1a3d053197411ed2546946d758a0
SonaliDas02 <30682458+SonaliDas02@users.noreply.github.com>
2017-08-16 15:00:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article -one',function(req,res){
+app.get('/article-one',function(req,res){
 res.sendFile('Article one has been requested and will be served ');
 });
 
-app.get('/article -two',function(req,res){
+app.get('/article-two',function(req,res){
 res.sendFile('Article two has been requested and will be served ');
 });
 
-app.get('/article -three',function(req,res){
+app.get('/article-three',function(req,res){
 res.sendFile('Article three has been requested and will be served ');
 });
 
