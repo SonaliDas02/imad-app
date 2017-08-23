43870369a5ec3e5697111b44b02ea10f9b57134e
SonaliDas02 <30682458+SonaliDas02@users.noreply.github.com>
2017-08-23 16:25:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById("main textt");
-element.innerHtml = "New Value";
+var element = document.getElementById("main text");
+element.innerHtml = "New Value"; 
