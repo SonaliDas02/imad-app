a3692cca8de8730ef91b8fcaa0ff0924fa8e9f95
SonaliDas02 <30682458+SonaliDas02@users.noreply.github.com>
2017-08-23 16:39:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ element.innerHTML = 'New Value';
 //Move the image
 
 var img =document.getElementById("Madi");
-img.onclick= function(){
+img.onclick = function(){
   img.style.marginLeft = "100 px";  
 };
