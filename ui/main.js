a27b7d4f5eb72dcfa6ab6026c4b5bcbff42a78f0
SonaliDas02 <30682458+SonaliDas02@users.noreply.github.com>
2017-08-30 12:53:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function () {
         //Not done yet
     };
     //Make the request
-   request.open("GET", "http://sonisonalidas.imad.hasura-app.io/counter", true);
+   request.open('GET', 'http://sonisonalidas.imad.hasura-app.io/counter', true);
    request.send(null);
 };
